31b4b8375e7955ee36bfe14fcafc8411b8680e97
kolad2 <koladik@gmail.com>
2023-12-19 21:21:45 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- get_start.py
+++ get_start.py
@@ -2,7 +2,7 @@ import os
 os.system('pip install numpy')
 os.system('pip install matplotlib')
 os.system('pip install opencv-python')
-#os.system("pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu115")
+os.system("pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118")
 
 """
     https://github.com/facebookresearch/segment-anything.git
